5ddceee4e1158ceccf20d7338e6d5c6d26cefbe5
vangeldergj <91121470+vangeldergj@users.noreply.github.com>
2023-10-20 13:03:56 +0200

    first commit

---
+++ client/src/components/index.js
@@ -0,0 +1,13 @@
+import CustomButton from "./CustomButton";
+import AIPick from "./AIPick";
+import ColorPicker from "./ColorPicker";
+import FilePicker from "./FilePicker";
+import Tab from "./Tab";
+
+export { 
+    CustomButton,
+    AIPick,
+    ColorPicker,
+    FilePicker,
+    Tab,
+ };
